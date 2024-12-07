08a3edd500619c7afae961ede2c036197aeda643
umedsingh98 <umedsingh0898@gmail.com>
2024-12-07 18:32:53 +0530

    updated

--- Frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ Frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -13,7 +13,7 @@ const FoodDisplay = ({category}) => {
         <h2>Top dishes near you</h2>
         <div className="food-display-list">
             {
-                food_list.map((item, index) => {
+                food_list?.map((item, index) => {
                   console.log(item);
                   if(category === "All" || category === item.category){
                     
